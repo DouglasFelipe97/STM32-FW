8c0fd7e1903973c31ae07a0925153ff14cb5cd6c
DouglasFelipe97 <86208209+DouglasFelipe97@users.noreply.github.com>
2024-02-20 11:42:43 -0300

    Add files via upload

---
+++ Bibliotecas/Biblio OLED STM32/OLED_Icons.h
@@ -0,0 +1,18 @@
+#ifndef ICONS_H_
+#define ICONS_H_
+
+#include <stdint.h>
+
+#define NULL_ICON (void*)0
+
+/* Icon in XBM */
+extern const uint8_t icon_clock[];
+extern const uint8_t icon_engrenagem[];
+extern const uint8_t logo[];
+extern const uint8_t logov2[] ;
+extern const uint8_t maxforte[];
+extern const uint8_t icon_chave[];
+extern const uint8_t icon_bateria[];
+
+
+#endif /* ICONS_H_ */
